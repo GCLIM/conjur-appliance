accfb5a78e2cc856e33f19b1931108a9971d6477
GCLIM <gclim.net@gmail.com>
2024-06-29 23:49:25 +0000

    remove load_credentials.sh

--- load_credentials.sh
+++
@@ -1,7 +0,0 @@
-export SSH_USERNAME=gclim
-export ADMIN_PASSWORD=MySecretP@ss1
-SSH_KEY=$(cat ~/.ssh/conjurappliance_ed25519)
-export SSH_PRIVATE_KEY=$SSH_KEY
-export WINRM_USERNAME=Administrator
-export WINRM_PASSWORD=MySecretP@ss1
-
