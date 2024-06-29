4cf4963752c50c71a3c246f9875b7af6380a86dd
gclim <gclim.net@gmail.com>
2024-06-30 08:26:33 +0900

    Support WinRM

--- conjur_orchestrator.py
+++ conjur_orchestrator.py
@@ -10,6 +10,8 @@ import asyncio
 import asyncssh
 import tracemalloc
 import logging
+import winrm
+import hvac
 
 # Set up logging
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
@@ -43,6 +45,36 @@ def print_announcement_banner(message):
     print(border)
 
 
+def get_vault_credentials():
+    """Fetch VAULT_URL from environment variables."""
+    vault_url = os.getenv('VAULT_URL')
+    if not vault_url:
+        raise ValueError("VAULT_URL environment variable not set.")
+    vault_api_key = os.getenv('VAULT_API_KEY')
+    if not vault_api_key:
+        raise ValueError("VAULT_API_KEY environment variable not set.")
+    return vault_url, vault_api_key
+
+
+def get_value_from_vault(key):
+
+    # authenticate to Vault
+    vault_url, vault_api_key = get_vault_credentials()
+    vault_client = hvac.Client(
+        url=vault_url,
+        token=vault_api_key
+    )
+
+    read_response = vault_client.secrets.kv.v2.read_secret_version(
+        raise_on_deleted_version=False,
+        mount_point="kv",
+        path="conjurorchestrator"
+    )
+
+    value = read_response["data"]["data"][key]
+    return value
+
+
 def get_admin_password():
     """Fetch ADMIN_PASSWORD from environment variables."""
     key = os.getenv('ADMIN_PASSWORD')
@@ -64,6 +96,21 @@ async def get_ssh_username():
     key = os.getenv('SSH_USERNAME')
     if not key:
         raise ValueError("SSH_USERNAME environment variable not set.")
+    return key
+
+def get_winrm_username():
+    """Fetch the WINRM_USERNAME from environment variables."""
+    key = os.getenv('WINRM_USERNAME')
+    if not key:
+        raise ValueError("WINRM_USERNAME environment variable not set.")
+    return key
+
+
+def get_winrm_password():
+    """Fetch the WINRM_PASSWORD from environment variables."""
+    key = os.getenv('WINRM_PASSWORD')
+    if not key:
+        raise ValueError("WINRM_PASSWORD environment variable not set.")
     return key
 
 
@@ -281,6 +328,31 @@ def get_followers_vars(yaml_file):
     return followers_vars
 
 
+def get_vars(asset_group, yaml_file):
+    """
+    A function to get the variables for the asset group from a YAML file.
+
+    Args:
+    - asset_group (str): The name of the asset group.
+    - yam_file (str): The path to the YAML file containing the asset group variables.
+
+    Returns:
+    - dict: The variables for the asset group.
+    """
+    # Read the YAML file
+    with open(yaml_file, 'r') as file:
+        yaml_dict = yaml.safe_load(file)
+
+    # Access the 'followers' section in the YAML dictionary
+    asset_group_section = yaml_dict.get(asset_group, {})
+
+    # Extract the 'vars' sub-section
+    asset_group_vars = asset_group_section.get('vars', {})
+
+    # Return the leader vars
+    return asset_group_vars
+
+
 # Function to get hostnames for leader and standbys
 def get_leader_cluster_hostnames(yaml_file):
     """
@@ -337,6 +409,23 @@ def get_follower_hostnames(yaml_file):
     follower_hostnames['followers'] = list(followers_section.keys())
 
     return follower_hostnames
+
+
+def get_vault_synchronizer_hostnames(yaml_file):
+    # Read the YAML file
+    with open(yaml_file, 'r') as file:
+        yaml_dict = yaml.safe_load(file)
+
+    # Initialize dictionaries to hold the hostnames
+    vaultsync_hostnames = {
+        'vaultsyncs': []
+    }
+
+    # Extract leader hostnames
+    vaultsyncs_section = yaml_dict.get('vaultsyncs', {}).get('hosts', {})
+    vaultsync_hostnames['vaultsyncs'] = list(vaultsyncs_section.keys())
+
+    return vaultsync_hostnames
 
 
 def resolve_current_hostname():
@@ -746,12 +835,239 @@ python3 conjur_appliance.py -m retire
         print(f"Follower retired.")
 
 
+def winrm_remote_shell_ps_script(hostname, ps_script):
+    # Create a WinRM session to the remote host using HTTPS and ignoring SSL certificate validation
+
+    # get winrm username
+    # username = get_winrm_username()
+    username =get_value_from_vault("WINRM_USERNAME")
+
+    # get winrm password
+    # password = get_winrm_password()
+    password = get_value_from_vault("WINRM_PASSWORD")
+
+    # Log the masked command
+    masked_command = conjur_appliance.mask_sensitive_info(ps_script)
+    logging.info(f"Executing PS command: {masked_command}")
+
+    try:
+        session = winrm.Session(
+            f'https://{hostname}:5986/wsman',
+            auth=(username, password),
+            server_cert_validation='ignore'  # Ignore SSL certificate validation
+        )
+        # Execute the command on the remote host
+        result = session.run_ps(ps_script)
+
+        # Print the output and error
+        if hasattr(result, 'std_out'):
+            logging.info(f"Output:\n{result.std_out.decode().strip()}")
+        if hasattr(result, 'std_err'):
+            if result.status_code == 0:
+                logging.info(f"Output:\n{result.std_err.decode().strip()}")
+            else:
+                logging.error(f"Error:\n{result.std_err.decode().strip()}")
+
+        # Check the output
+        if result.status_code == 0:
+            return result.std_out.decode().strip()
+        else:
+            return f"Error: {result.std_err.decode().strip()}"
+
+    except winrm.exceptions.TransportError as e:
+
+        logging.error(f"WinRM connection failed: {str(e)}")
+
+        if hasattr(e, 'returncode'):
+            logging.error(f"Exit status: {e.returncode}")
+
+        if hasattr(e, 'cmd'):
+            masked_cmd = conjur_appliance.mask_sensitive_info(e.cmd)
+            logging.error(f"Command: {masked_cmd}")
+
+        if hasattr(e, 'stdout') and e.stdout:
+            logging.error(f"Standard output:\n{e.std_out.decode().strip()}")
+
+        if hasattr(e, 'stderr') and e.stderr:
+            logging.error(f"Standard error:\n{e.std_err.decode().strip()}")
+
+        raise  # Re-raise the exception for higher-level handling
+
+
+def check_dotnet_framework_48(hostname):
+    # PowerShell command to check the .NET Framework 4.8 Release value
+    ps_script = '''
+    $ReleaseKey = Get-ItemProperty -Path "HKLM:\\SOFTWARE\\Microsoft\\NET Framework Setup\\NDP\\v4\\Full" -Name Release
+    if ($ReleaseKey.Release -ge 528040) {
+        Write-Output "Installed"
+    } else {
+        Write-Output "Not Installed"
+    }
+    '''
+    return winrm_remote_shell_ps_script(hostname, ps_script)
+
+
+def remote_write_silent_init_file(yaml_file, hostname):
+    # get vault syncs vars
+    vaultsyncs_vars = get_vars('vaultsyncs', yaml_file)
+    # PowerShell command script to create the silent.ini file
+    ps_script = f'''
+$silentIniPath = "{vaultsyncs_vars['Sync_Package_Directory']}\\\silent.ini"
+
+$newContent = @"
+##############################################
+### All the following values are mandatory ###
+##############################################
+
+[Main]
+
+# Specify the target installation path for the Vault Synchronizer
+InstallationTargetPath={vaultsyncs_vars['InstallationTargetPath']}
+
+# Specify the URL of the PVWA, starting with https:// and excluding the full path
+PVWAURL={vaultsyncs_vars['PVWAURL']}
+
+# Specify Vault address. If the Vault has multiple addresses, list them separated by commas and without spaces
+VaultAddress={vaultsyncs_vars['VaultAddress']}
+
+# Specify Vault port (default=1858)
+VaultPort={vaultsyncs_vars['VaultPort']}
+
+# Specify Vault name (alias)
+VaultName={vaultsyncs_vars['VaultName']}
+
+# Specify the name of the Safe for storing accounts used to manage this Synchronizer
+SyncSafeName={vaultsyncs_vars['SyncSafeName']}
+
+# Enter the Conjur Enterprise hostname and port (port is optional) in the format of https://hostname[:port]
+# Enter the Conjur Cloud API URL that you copied from the Conjur Cloud UI
+ConjurServerDNS={vaultsyncs_vars['ConjurServerDNS']}
+
+# Specify Conjur Enterprise account name
+# For Conjur Cloud, this parameter is always set to 'conjur'
+ConjurAccount={vaultsyncs_vars['ConjurAccount']}
+
+# Specify the full path to the Conjur admin credentials file, created before the installation (only for PAS earlier than v11.4)
+ConjurCredentialsFilePath={vaultsyncs_vars['ConjurCredentialsFilePath']}
+
+# Specify LOB name (only for PAS v11.4 and later / Privilege Cloud)
+LOBName={vaultsyncs_vars['LOBName']}
+
+# Specify 'CyberArk Vault' platform used by the LOB account (only for PAS v11.4 and later / Privilege Cloud)
+LOBPlatform={vaultsyncs_vars['LOBPlatform']}
+
+# Specify if Synchronizer is running in multi-node mode (set false for Conjur Cloud integration!)
+MultiNodeEnabled={vaultsyncs_vars['MultiNodeEnabled']}
+
+# Specify Cluster key (keep empty for Conjur Cloud integration!)
+ClusterKey={vaultsyncs_vars['ClusterKey']}
+
+# Are you installing the Vault Synchronizer for Conjur Cloud (enter true/false)?
+ConjurCloudSelected={vaultsyncs_vars['ConjurCloudSelected']}
+
+# Enter the Vault Synchronizer credentials that you copied from the Conjur Cloud UI
+ConjurCloudHostAndToken={vaultsyncs_vars['ConjurCloudHostAndToken']}
+"@
+# echo silentIni_File_Path: 
+# echo $silentIniPath
+# echo Write_to_silent_ini_file:
+Set-Content -Path $silentIniPath -Value $newContent -Force
+# Get-Content -Path $silentIniPath
+'''
+    result = winrm_remote_shell_ps_script(hostname, ps_script)
+    return result
+
+def remote_install_vault_synchronizer(yaml_file, hostname):
+
+    # get vault syncs vars
+    vaultsyncs_vars = get_vars('vaultsyncs', yaml_file)
+    # Need to read from Password Vault
+    vault_admin = get_value_from_vault("VAULT_ADMIN")
+    vault_password = get_value_from_vault("VAULT_PASSWORD")
+    conjur_admin_password = get_value_from_vault("CONJUR_ADMIN_PASSWORD")
+
+    # PowerShell command script to create the PSCredential object
+    ps_script = f'''
+# Define the username as a plain text string
+$vault_admin_username = "{vault_admin}"
+
+# Define the password as a plain text string
+$vault_admin_password = "{vault_password}"
+
+# Convert the plain text password to a SecureString
+$vault_securePassword = ConvertTo-SecureString -String $vault_admin_password -AsPlainText -Force
+
+# Create the PSCredential object using the username and secure password
+$PVWACredentials = New-Object System.Management.Automation.PSCredential ($vault_admin_username, $vault_securePassword)
+
+# Output the credentials object to verify (for demonstration)
+$PVWACredentials
+
+# Define the username as a plain text string
+$conjur_admin_username = "admin"
+
+# Define the password as a plain text string
+$conjur_admin_password = "{conjur_admin_password}"
+
+# Convert the plain text password to a SecureString
+$conjur_securePassword = ConvertTo-SecureString -String $conjur_admin_password -AsPlainText -Force
+
+# Create the PSCredential object using the username and secure password
+$conjurCredentials = New-Object System.Management.Automation.PSCredential ($conjur_admin_username, $conjur_securePassword)
+
+# Output the credentials object to verify (for demonstration)
+$conjurCredentials
+
+Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process -Force
+
+cd {vaultsyncs_vars['Sync_Package_Directory']}
+
+.\\V5SynchronizerInstallation.ps1 -silent -trustPVWAAndConjurCert
+'''
+    result = winrm_remote_shell_ps_script(hostname, ps_script)
+    return result
+
+
+def deploy_vaultsync_model(yaml_file):
+    """
+    Deploys a vaultsync model based on the provided YAML file.
+
+    Parameters:
+    - yaml_file: the YAML file containing configuration information
+
+    Returns:
+    - None
+    """
+    vaultsyncs_vars = get_vars('vaultsyncs', yaml_file)
+    hostnames = get_vault_synchronizer_hostnames(yaml_file)
+    username = vaultsyncs_vars['ansible_user']
+    password = vaultsyncs_vars['ansible_password']
+
+    for hostname in hostnames['vaultsyncs']:
+        print_announcement_banner(f"Deploying Vault Synchronizer on {hostname}")
+        logging.info("Precheck...")
+        result = check_dotnet_framework_48(hostname)
+        if result == "Installed":
+            logging.info(".Net Framework 4.8 is installed.")
+            logging.info("Precheck...Passed")
+            logging.info("Write Silent.ini for Vault Synchronizer...")
+            result = remote_write_silent_init_file(yaml_file, hostname)
+            # print(result)
+            logging.info("Install Vault Synchronizer...")
+            result = remote_install_vault_synchronizer(yaml_file, hostname)
+        else:
+            logging.error(f".Net Framework 4.8 is not installed.")
+            logging.error("Precheck...Failed")
+    return
+
+
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description="Conjur deployment orchestrator",
                                      formatter_class=argparse.RawTextHelpFormatter,
                                      add_help=True)
-    parser.add_argument("-d", "--deploy", type=str, help="leader: deploy leader cluster\nfollower: deploy followers")
+    parser.add_argument("-d", "--deploy", type=str,
+                        help="leader: deploy leader cluster\nfollower: deploy followers\nvaultsync: deploy vault ynchronizer")
     parser.add_argument("-o", "--orchestrator", type=str, help="leader: orchestrator leader cluster deployment")
     parser.add_argument("-r", "--retire", type=str, help="leader: retire leader cluster\nfollower: retire followers")
     parser.add_argument("-i", "--inventory", type=str, help="eg. inventories/dev.yml")
@@ -810,6 +1126,18 @@ if __name__ == "__main__":
             exit(1)
         deploy_follower_model(args.inventory)
 
+    if args.deploy in ["vaultsync"]:
+        # check if inventory exist for arg.inventory
+        if not args.inventory:
+            parser.print_help()
+            print("Error: -i, --inventory cannot be empty.")
+            exit(1)
+        # check if inventory exist on the disk
+        if not os.path.exists(args.inventory):
+            print(f"Error: {args.inventory} does not exist.")
+            exit(1)
+        deploy_vaultsync_model(args.inventory)
+
     if args.retire in ["follower"]:
         # check if inventory exist for arg.inventory
         if not args.inventory:
@@ -827,4 +1155,3 @@ if __name__ == "__main__":
         health_check_url = "http://conjur01.gcloud101.com:444/health"
         # Fetch and analyze the health data
         conjur_appliance.fetch_and_analyze_health(health_check_url)
-
--- verifynet48.py
+++ verifynet48.py
@@ -1,6 +1,7 @@
 import winrm
 # from requests.packages.urllib3.exceptions import InsecureRequestWarning
-import requests
+# import requests
+
 
 # # Disable SSL warnings
 # requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
@@ -11,10 +12,10 @@ def check_dotnet_framework_48(host, username, password):
         session = winrm.Session(
             f'https://{host}:5986/wsman',
             auth=(username, password),
-            transport='ntlm',
+            # transport='ntlm',
             server_cert_validation='ignore'  # Ignore SSL certificate validation
         )
-        
+
         # PowerShell command to check the .NET Framework 4.8 Release value
         ps_script = '''
         $ReleaseKey = Get-ItemProperty -Path "HKLM:\\SOFTWARE\\Microsoft\\NET Framework Setup\\NDP\\v4\\Full" -Name Release
@@ -24,18 +25,19 @@ def check_dotnet_framework_48(host, username, password):
             Write-Output "Not Installed"
         }
         '''
-        
+
         # Execute the command on the remote host
         result = session.run_ps(ps_script)
-        
+
         # Check the output
         if result.status_code == 0:
             return result.std_out.decode().strip()
         else:
             return f"Error: {result.std_err.decode().strip()}"
-    
+
     except Exception as e:
         return f"Exception: {str(e)}"
+
 
 # Replace with your remote host details
 host = "vaultsyn.gcloud101.com"
